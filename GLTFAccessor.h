da874b37e4fc3511846217341062a83026a93257
Joseph Cunningham <joechamm@outlook.com>
2022-09-20 08:39:38 -0400

    templates for accessor

--- GLTFAccessor.h
+++ GLTFAccessor.h
@@ -31,91 +31,439 @@ SOFTWARE.
 
 #include <QObject>
 #include <QJsonObject>
+#include <QJsonArray>
+#include <QList>
+#include <array>
 
 namespace jcqt
 {
-	class GLTFAccessor : public QObject
+	enum class eAccessorComponentType : qint16
 	{
-		Q_OBJECT
+		eComponentType_BYTE = 5120,
+		eComponentType_UNSIGNED_BYTE = 5121,
+		eComponentType_SHORT = 5122,
+		eComponentType_UNSIGNED_SHORT = 5123,
+		eComponentType_UNSIGNED_INT = 5125,
+		eComponentType_FLOAT = 5126,
+		eComponentType_INVALID = 9999
+	};
 
-	public:
-		struct SparseValues
-		{
-			qsizetype m_bufferViewIndex;
-			qsizetype m_byteOffset;
-		};
+	enum class eAccessorType : qint8
+	{
+		eType_SCALAR = 1,
+		eType_VEC2 = 2,
+		eType_VEC3 = 3,
+		eType_VEC4 = 4,
+		eType_MAT2 = 5,
+		eType_MAT3 = 6,
+		eType_MAT4 = 7,
+		eType_INVALID = 8
+	};
 
-		struct SparseIndices
-		{
-			qsizetype m_bufferViewIndex;
-			qsizetype m_byteOffset;
-			qsizetype m_componentType;
-		};
+	struct SparseValues
+	{
+		qsizetype m_bufferViewIndex = -1;
+		qsizetype m_byteOffset = 0;
+	};
 
-		struct AccessorSparse
-		{
-			qsizetype		m_count;
-			SparseValues	m_values;
-			SparseIndices	m_indices;
-			// contains all the extras and extensions for sparse values and sparse indices
-			QJsonObject		m_jsonObject; 
-		};
+	struct SparseIndices
+	{
+		qsizetype m_bufferViewIndex = -1;
+		qsizetype m_byteOffset = 0;
+		eAccessorComponentType m_componentType = eAccessorComponentType::eComponentType_INVALID;
+	};
 
-	public:
-		GLTFAccessor ( QObject* parent );
-		~GLTFAccessor ();
-
-		/*
-			https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html#reference-bufferview
-			'bufferView' requires the 'buffer' field and 'byteLength' field to be present
-		*/
-		static bool isValidJson ( const QJsonObject& jsonObj );
-		static qsizetype getBufferIndexFromJson ( const QJsonObject& jsonObj );
-		static qsizetype getByteOffsetFromJson ( const QJsonObject& jsonObj );
-		static qsizetype getByteLengthFromJson ( const QJsonObject& jsonObj );
-		static qsizetype getByteStrideFromJson ( const QJsonObject& jsonObj );
-		static qsizetype getTargetFromJson ( const QJsonObject& jsonObj );
-		static QString getNameFromJson ( const QJsonObject& jsonObj );
-		static QJsonObject getExtensionsFromJson ( const QJsonObject& jsonObj );
-		static QJsonObject getExtrasFromJson ( const QJsonObject& jsonObj );
-
-		static SparseValues createSparseValuesFromJson ( const QJsonObject& jsonObj );
-		static SparseIndices createSparseIndicesFromJson ( const QJsonObject& jsonObj );
-		static AccessorSparse createAccessorSparseFromJson ( const QJsonObject& jsonObj );
-
-		bool isValid () const;
-		bool hasByteOffset () const;
-		bool hasByteStride () const;
-		bool hasTarget () const;
-		bool hasName () const;
-		bool hasExtensions () const;
-		bool hasExtras () const;
-
-		qsizetype bufferIndex () const;
-		qsizetype byteOffset () const;
-		qsizetype byteLength () const;
-		qsizetype byteStride () const;
-		qsizetype target () const;
-
-		QString name () const;
-		QJsonObject extensions () const;
-		QJsonObject extras () const;
-
-		bool setJsonObject ( const QJsonObject& jsonObj );
-		bool setValuesFromJson ();
-
-	private:
-		qsizetype		m_bufferViewIndex;
-		qsizetype		m_byteOffset;
-		qsizetype		m_componentType;
-		qsizetype		m_count;
-		qsizetype		m_max;
-		qsizetype		m_min;
-		bool			m_normalized;
-		AccessorSparse	m_sparse;
+	struct AccessorSparse
+	{
+		qsizetype		m_count = -1;
+		SparseValues	m_values;
+		SparseIndices	m_indices;
+		// contains all the extras and extensions for sparse values and sparse indices
 		QJsonObject		m_jsonObject;
 	};
 
+	struct Accessor
+	{
+		qsizetype					m_bufferViewIndex = -1;
+		qsizetype					m_byteOffset = 0;
+		eAccessorComponentType		m_componentType = eAccessorComponentType::eComponentType_INVALID;
+		qsizetype					m_count = -1;
+		eAccessorType				m_type = eAccessorType::eType_INVALID;
+		bool						m_normalized = false;
+		AccessorSparse				m_sparse;
+		QJsonObject					m_jsonObject;		
+	};
+
+	SparseValues createSparseValuesFromJson ( const QJsonObject& jsonObj )
+	{
+		SparseValues sparseValues;
+		sparseValues.m_bufferViewIndex = jsonObj.value ( "bufferView" ).toInteger ();
+		sparseValues.m_byteOffset = jsonObj.contains ( "byteOffset" ) ? jsonObj.value ( "byteOffset" ).toInteger () : 0;
+		return sparseValues;
+	}
+
+	SparseIndices createSparseIndicesFromJson ( const QJsonObject& jsonObj )
+	{
+		SparseIndices sparseIndices;
+		sparseIndices.m_bufferViewIndex = jsonObj.value ( "bufferView" ).toInteger ();
+		qint64 compType = jsonObj.value ( "componentType" ).toInteger ();
+		switch ( compType )
+		{	
+		case 5121:
+			sparseIndices.m_componentType = eAccessorComponentType::eComponentType_UNSIGNED_BYTE;
+			break;		
+		case 5123:
+			sparseIndices.m_componentType = eAccessorComponentType::eComponentType_UNSIGNED_SHORT;
+			break;
+		case 5125:
+			sparseIndices.m_componentType = eAccessorComponentType::eComponentType_UNSIGNED_INT;
+			break;		
+		default:
+			sparseIndices.m_componentType = eAccessorComponentType::eComponentType_INVALID;
+			break;
+		}
+
+		sparseIndices.m_byteOffset = jsonObj.contains ( "byteOffset" ) ? jsonObj.value ( "byteOffset" ).toInteger () : 0;
+		return sparseIndices;
+	}
+
+	AccessorSparse createAccessorSparseFromJson ( const QJsonObject& jsonObj )
+	{
+		AccessorSparse accessorSparse;
+		accessorSparse.m_count = jsonObj.value ( "count" ).toInteger ();
+		accessorSparse.m_indices = createSparseIndicesFromJson ( jsonObj.value ( "indices" ).toObject () );
+		accessorSparse.m_values = createSparseValuesFromJson ( jsonObj.value ( "values" ).toObject () );
+		accessorSparse.m_jsonObject = jsonObj;
+		return accessorSparse;
+	}
+
+	Accessor createAccessorFromJson ( const QJsonObject& jsonObj )
+	{
+		Accessor accessor;
+		accessor.m_bufferViewIndex = jsonObj.contains ( "bufferView" ) ? jsonObj.value ( "bufferView" ).toInteger () : -1;
+		accessor.m_byteOffset = jsonObj.contains ( "byteOffset" ) ? jsonObj.value ( "byteOffset" ).toInteger () : 0;
+
+		qint64 compType = jsonObj.value ( "componentType" ).toInteger ();
+		switch ( compType )
+		{
+		case 5120:
+			accessor.m_componentType = eAccessorComponentType::eComponentType_BYTE;
+			break;
+		case 5121:
+			accessor.m_componentType = eAccessorComponentType::eComponentType_UNSIGNED_BYTE;
+			break;
+		case 5122:
+			accessor.m_componentType = eAccessorComponentType::eComponentType_SHORT;
+			break;
+		case 5123:
+			accessor.m_componentType = eAccessorComponentType::eComponentType_UNSIGNED_SHORT;
+			break;		
+		case 5125:
+			accessor.m_componentType = eAccessorComponentType::eComponentType_UNSIGNED_INT;
+			break;
+		case 5126:
+			accessor.m_componentType = eAccessorComponentType::eComponentType_FLOAT;
+			break;
+		default:
+			accessor.m_componentType = eAccessorComponentType::eComponentType_INVALID;
+			break;
+		}
+
+		accessor.m_normalized = jsonObj.value ( "normalized" ).toBool ( false );
+		accessor.m_count = jsonObj.value ( "count" ).toInteger ();
+
+		QString type = jsonObj.value ( "type" ).toString ();
+		
+		if ( type.compare ( "SCALAR", Qt::CaseInsensitive ) == 0 )
+		{
+			accessor.m_type = eAccessorType::eType_SCALAR;
+		}
+		else if ( type.compare ( "VEC2", Qt::CaseInsensitive ) == 0 )
+		{
+			accessor.m_type = eAccessorType::eType_VEC2;
+		}
+		else if ( type.compare ( "VEC3", Qt::CaseInsensitive ) == 0 )
+		{
+			accessor.m_type = eAccessorType::eType_VEC3;
+		}
+		else if ( type.compare ( "VEC4", Qt::CaseInsensitive ) == 0 )
+		{
+			accessor.m_type = eAccessorType::eType_VEC4;
+		}
+		else if ( type.compare ( "MAT2", Qt::CaseInsensitive ) == 0 )
+		{
+			accessor.m_type = eAccessorType::eType_MAT2;
+		}
+		else if ( type.compare ( "MAT3", Qt::CaseInsensitive ) == 0 )
+		{
+			accessor.m_type = eAccessorType::eType_MAT3;
+		}
+		else if ( type.compare ( "MAT4", Qt::CaseInsensitive ) == 0 )
+		{
+			accessor.m_type = eAccessorType::eType_MAT4;
+		}
+		else
+		{
+			accessor.m_type = eAccessorType::eType_INVALID;
+		}
+
+		if ( jsonObj.contains ( "sparse" ) )
+		{
+			accessor.m_sparse = createAccessorSparseFromJson ( jsonObj.value ( "sparse" ).toObject () );
+		}
+
+		accessor.m_jsonObject = jsonObj;
+		return accessor;		
+	}
+
+	//QList<std::array<float, 2>> getAccessorFloatMinMax ( const QJsonObject& jsonObj )
+	//{
+	//	QList<std::array<float, 2>> minMaxList;
+	//	bool hasMinMax = jsonObj.contains ( "min" ) && jsonObj.contains ( "max" );
+	//	if ( !hasMinMax )
+	//	{
+	//		qWarning () << "MIN/MAX NOT AVAILABLE. RETURNING EMPTY LIST" << Qt::endl;
+	//		return minMaxList;
+	//	}
+
+	//	quint64 compType = jsonObj.value ( "componentType" ).toInteger ();
+	//	if ( compType != 5126 /* FLOAT */ )
+	//	{
+	//		qWarning () << "COMPONENT TYPE ISN'T FLOAT. RETURNING EMPTY LIST" << Qt::endl;
+	//		return minMaxList;
+	//	}
+
+	//	QJsonArray minList = jsonObj.value ( "min" ).toArray ();
+	//	QJsonArray maxList = jsonObj.value ( "max" ).toArray ();
+	//	QString type = jsonObj.value ( "type" ).toString ();
+	//	
+	//	if ( type.compare ( "SCALAR", Qt::CaseInsensitive ) == 0 )
+	//	{
+	//		constexpr qsizetype COMP_COUNT = 1;
+	//		minMaxList.resize ( COMP_COUNT );
+	//		for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+	//		{
+	//			float minVal = static_cast< float >( minList.at ( i ).toDouble () );
+	//			float maxVal = static_cast< float >( maxList.at ( i ).toDouble () );
+	//			minMaxList [ i ] = std::array<float, 2> ( { minVal, maxVal } );
+	//		}
+	//	}
+	//	else if ( type.compare ( "VEC2", Qt::CaseInsensitive ) == 0 )
+	//	{
+	//		constexpr qsizetype COMP_COUNT = 2;
+	//		minMaxList.resize ( COMP_COUNT );
+	//		for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+	//		{
+	//			float minVal = static_cast< float >( minList.at ( i ).toDouble () );
+	//			float maxVal = static_cast< float >( maxList.at ( i ).toDouble () );
+	//			minMaxList [ i ] = std::array<float, 2> ( { minVal, maxVal } );
+	//		}
+	//	}
+	//	else if ( type.compare ( "VEC3", Qt::CaseInsensitive ) == 0 )
+	//	{
+	//		constexpr qsizetype COMP_COUNT = 3;
+	//		minMaxList.resize ( COMP_COUNT );
+	//		for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+	//		{
+	//			float minVal = static_cast< float >( minList.at ( i ).toDouble () );
+	//			float maxVal = static_cast< float >( maxList.at ( i ).toDouble () );
+	//			minMaxList [ i ] = std::array<float, 2> ( { minVal, maxVal } );
+	//		}
+	//	}
+	//	else if ( type.compare ( "VEC4", Qt::CaseInsensitive ) == 0 )
+	//	{
+	//		constexpr qsizetype COMP_COUNT = 4;
+	//		minMaxList.resize ( COMP_COUNT );
+	//		for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+	//		{
+	//			float minVal = static_cast< float >( minList.at ( i ).toDouble () );
+	//			float maxVal = static_cast< float >( maxList.at ( i ).toDouble () );
+	//			minMaxList [ i ] = std::array<float, 2> ( { minVal, maxVal } );
+	//		}
+	//	}
+	//	else if ( type.compare ( "MAT2", Qt::CaseInsensitive ) == 0 )
+	//	{
+	//		constexpr qsizetype COMP_COUNT = 4;
+	//		minMaxList.resize ( COMP_COUNT );
+	//		for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+	//		{
+	//			float minVal = static_cast< float >( minList.at ( i ).toDouble () );
+	//			float maxVal = static_cast< float >( maxList.at ( i ).toDouble () );
+	//			minMaxList [ i ] = std::array<float, 2> ( { minVal, maxVal } );
+	//		}
+	//	}
+	//	else if ( type.compare ( "MAT3", Qt::CaseInsensitive ) == 0 )
+	//	{
+	//		constexpr qsizetype COMP_COUNT = 9;
+	//		minMaxList.resize ( COMP_COUNT );
+	//		for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+	//		{
+	//			float minVal = static_cast< float >( minList.at ( i ).toDouble () );
+	//			float maxVal = static_cast< float >( maxList.at ( i ).toDouble () );
+	//			minMaxList [ i ] = std::array<float, 2> ( { minVal, maxVal } );
+	//		}
+	//	}
+	//	else if ( type.compare ( "MAT4", Qt::CaseInsensitive ) == 0 )
+	//	{
+	//		constexpr qsizetype COMP_COUNT = 16;
+	//		minMaxList.resize ( COMP_COUNT );
+	//		for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+	//		{
+	//			float minVal = static_cast< float >( minList.at ( i ).toDouble () );
+	//			float maxVal = static_cast< float >( maxList.at ( i ).toDouble () );
+	//			minMaxList [ i ] = std::array<float, 2> ( { minVal, maxVal } );
+	//		}
+	//	}
+
+	//	return minMaxList;
+	//}
+
+	template<typename T>
+	QList<std::array<T, 2>> getAccessorMinMax ( const QJsonObject& jsonObj )
+	{
+		QList<std::array<T, 2>> minMaxList;
+		bool hasMinMax = jsonObj.contains ( "min" ) && jsonObj.contains ( "max" );
+		if ( !hasMinMax )
+		{
+			qWarning () << "MIN/MAX NOT AVAILABLE. RETURNING EMPTY LIST" << Qt::endl;
+			return minMaxList;
+		}
+
+		quint64 compType = jsonObj.value ( "componentType" ).toInteger ();
+		switch ( compType )
+		{
+		case 5120: // BYTE
+			if ( !( std::is_same<T, qint8>::value ) )
+			{
+				qWarning () << "COMPONENT TYPE MISMATCH. RETURNING EMPTY LIST" << Qt::endl;
+				return minMaxList;
+			}
+			break;
+		case 5121: // UNSIGNED_BYTE
+			if ( !( std::is_same<T, quint8>::value ) )
+			{
+				qWarning () << "COMPONENT TYPE MISMATCH. RETURNING EMPTY LIST" << Qt::endl;
+				return minMaxList;
+			}
+			break;
+		case 5122: // SHORT
+			if ( !( std::is_same<T, qint16>::value ) )
+			{
+				qWarning () << "COMPONENT TYPE MISMATCH. RETURNING EMPTY LIST" << Qt::endl;
+				return minMaxList;
+			}
+			break;
+		case 5123: // UNSIGNED_SHORT
+			if ( !( std::is_same<T, quint16>::value ) )
+			{
+				qWarning () << "COMPONENT TYPE MISMATCH. RETURNING EMPTY LIST" << Qt::endl;
+				return minMaxList;
+			}
+			break;
+		case 5125: // UNSIGNED_INT
+			if ( !( std::is_same<T, quint32>::value ) )
+			{
+				qWarning () << "COMPONENT TYPE MISMATCH. RETURNING EMPTY LIST" << Qt::endl;
+				return minMaxList;
+			}
+			break;
+		case 5126: // FLOAT
+			if ( !( std::is_same<T, float>::value ) )
+			{
+				qWarning () << "COMPONENT TYPE MISMATCH. RETURNING EMPTY LIST" << Qt::endl;
+				return minMaxList;
+			}
+			break;
+		default:
+			qWarning () << "INVALID COMPONENT TYPE. RETURNING EMPTY LIST" << Qt::endl;
+			return minMaxList;
+			break;
+		}
+				
+		QJsonArray minList = jsonObj.value ( "min" ).toArray ();
+		QJsonArray maxList = jsonObj.value ( "max" ).toArray ();
+		QString type = jsonObj.value ( "type" ).toString ();
+
+		if ( type.compare ( "SCALAR", Qt::CaseInsensitive ) == 0 )
+		{
+			constexpr qsizetype COMP_COUNT = 1;
+			minMaxList.resize ( COMP_COUNT );
+			for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+			{
+				T minVal = static_cast< T >( minList.at ( i ).toDouble () );
+				T maxVal = static_cast< T >( maxList.at ( i ).toDouble () );
+				minMaxList [ i ] = std::array<T, 2> ( { minVal, maxVal } );
+			}
+		}
+		else if ( type.compare ( "VEC2", Qt::CaseInsensitive ) == 0 )
+		{
+			constexpr qsizetype COMP_COUNT = 2;
+			minMaxList.resize ( COMP_COUNT );
+			for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+			{
+				T minVal = static_cast< T >( minList.at ( i ).toDouble () );
+				T maxVal = static_cast< T >( maxList.at ( i ).toDouble () );
+				minMaxList [ i ] = std::array<T, 2> ( { minVal, maxVal } );
+			}
+		}
+		else if ( type.compare ( "VEC3", Qt::CaseInsensitive ) == 0 )
+		{
+			constexpr qsizetype COMP_COUNT = 3;
+			minMaxList.resize ( COMP_COUNT );
+			for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+			{
+				T minVal = static_cast< T >( minList.at ( i ).toDouble () );
+				T maxVal = static_cast< T >( maxList.at ( i ).toDouble () );
+				minMaxList [ i ] = std::array<T, 2> ( { minVal, maxVal } );
+			}
+		}
+		else if ( type.compare ( "VEC4", Qt::CaseInsensitive ) == 0 )
+		{
+			constexpr qsizetype COMP_COUNT = 4;
+			minMaxList.resize ( COMP_COUNT );
+			for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+			{
+				T minVal = static_cast< T >( minList.at ( i ).toDouble () );
+				T maxVal = static_cast< T >( maxList.at ( i ).toDouble () );
+				minMaxList [ i ] = std::array<T, 2> ( { minVal, maxVal } );
+			}
+		}
+		else if ( type.compare ( "MAT2", Qt::CaseInsensitive ) == 0 )
+		{
+			constexpr qsizetype COMP_COUNT = 4;
+			minMaxList.resize ( COMP_COUNT );
+			for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+			{
+				T minVal = static_cast< T >( minList.at ( i ).toDouble () );
+				T maxVal = static_cast< T >( maxList.at ( i ).toDouble () );
+				minMaxList [ i ] = std::array<T, 2> ( { minVal, maxVal } );
+			}
+		}
+		else if ( type.compare ( "MAT3", Qt::CaseInsensitive ) == 0 )
+		{
+			constexpr qsizetype COMP_COUNT = 9;
+			minMaxList.resize ( COMP_COUNT );
+			for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+			{
+				T minVal = static_cast< T >( minList.at ( i ).toDouble () );
+				T maxVal = static_cast< T >( maxList.at ( i ).toDouble () );
+				minMaxList [ i ] = std::array<T, 2> ( { minVal, maxVal } );
+			}
+		}
+		else if ( type.compare ( "MAT4", Qt::CaseInsensitive ) == 0 )
+		{
+			constexpr qsizetype COMP_COUNT = 16;
+			minMaxList.resize ( COMP_COUNT );
+			for ( qsizetype i = 0; i < COMP_COUNT; i++ )
+			{
+				T minVal = static_cast< T >( minList.at ( i ).toDouble () );
+				T maxVal = static_cast< T >( maxList.at ( i ).toDouble () );
+				minMaxList [ i ] = std::array<T, 2> ( { minVal, maxVal } );
+			}
+		}
+
+		return minMaxList;
+	}
 }
 
 #endif // !__GLTF_ACCESSOR_H__

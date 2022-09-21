dcaf127f9a65cc1c6144e96c7f425fb2689343a2
Joseph Cunningham <joechamm@outlook.com>
2022-09-21 03:02:03 -0400

    buffer view struct (much simpler i think)

--- GLTFBufferView.h
+++ GLTFBufferView.h
@@ -34,6 +34,54 @@ SOFTWARE.
 
 namespace jcqt
 {
+	enum class eBufferViewTarget : quint16
+	{
+		eBufferViewTarget_NOT_SET = 1,
+		eBufferViewTarget_ARRAY_BUFFER = 34962,
+		eBufferViewTarget_ELEMENT_ARRAY_BUFFER = 34963,
+		eBufferViewTarget_INVALID = 9999
+	};
+
+	struct BufferView
+	{
+		qsizetype m_bufferIndex = -1;
+		qsizetype m_byteOffset = 0;
+		qsizetype m_byteLength = - 1;
+		qsizetype m_byteStride = - 1;
+		eBufferViewTarget m_target = eBufferViewTarget::eBufferViewTarget_INVALID;
+		QJsonObject m_jsonObject;
+	};
+
+	BufferView createBufferViewFromJson ( const QJsonObject& jsonObj )
+	{
+		BufferView bufferView;
+		bufferView.m_bufferIndex = jsonObj.value ( "buffer" ).toInteger ();
+		bufferView.m_byteLength = jsonObj.value ( "byteLength" ).toInteger ();
+		bufferView.m_byteOffset = jsonObj.contains ( "byteOffset" ) ? jsonObj.value ( "byteOffset" ).toInteger () : 0;
+		bufferView.m_byteStride = jsonObj.contains ( "byteStride" ) ? jsonObj.value ( "byteStride" ).toInteger () : 0;
+		
+		quint64 target = jsonObj.contains ( "target" ) ? jsonObj.value ( "target" ).toInteger () : 0;
+		switch ( target )
+		{
+		case 0: // NOT SET
+			bufferView.m_target = eBufferViewTarget::eBufferViewTarget_NOT_SET;
+			break;
+		case 34962: // ARRAY_BUFFER
+			bufferView.m_target = eBufferViewTarget::eBufferViewTarget_ARRAY_BUFFER;
+			break;
+		case 34963: // ELEMENT_ARRAY_BUFFER
+			bufferView.m_target = eBufferViewTarget::eBufferViewTarget_ELEMENT_ARRAY_BUFFER;
+			break;
+		default:
+			bufferView.m_target = eBufferViewTarget::eBufferViewTarget_INVALID;
+			break;
+		}
+
+		bufferView.m_jsonObject = jsonObj;
+		return bufferView;
+	}
+
+
 	class GLTFBufferView : public QObject
 	{
 		Q_OBJECT
